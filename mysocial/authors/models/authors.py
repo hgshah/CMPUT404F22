04bef847909a841dfa2e7ffbad8b3644ec1a700f
turnipxenon <turnipxenon@gmail.com>
2022-10-16 18:30:30 -0600

    Add basic fields for Author

--- mysocial/authors/models/authors.py
+++ mysocial/authors/models/authors.py
@@ -1,6 +1,30 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .author_manager import AuthorManager
 
 
 class Author(AbstractUser):
+    """
+    Fields:
+    - Inherited fields not shown below:
+        :ivar id: auto generated BigInt
+        :ivar password: str; encrypted
+        :ivar last_login: datetime
+        :ivar is_superuser: bool
+        :ivar username: str; unique constraint
+        :ivar email: str; unique constraint
+        :ivar is_staff: bool
+        :ivar is_active: bool
+        :ivar date_joined: datetime
+    """
+
+    # Remove this unnecessary fields
+    first_name = None
+    last_name = None
+
+    display_name = models.TextField(blank=True)
+    github = models.TextField(blank=True)
+    # todo(turnip): uncomment when ready
+    # profile_image = ???
+
     objects = AuthorManager()
