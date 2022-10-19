a50d7f92b8108ce0766ef93df0155b85ebb90a8b
Amanda Nguyen <amanda6@ualberta.ca>
2022-10-19 15:50:09 -0600

    completed get all public posts

--- mysocial/post/urls.py
+++ mysocial/post/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('posts/public/', views.PublicPostView.as_view()),
     path('authors/<uuid:author_id>/posts/', views.CreationPostView.as_view()),
 ]
--- mysocial/post/views.py
+++ mysocial/post/views.py
@@ -1,13 +1,30 @@
-from django.http.response import HttpResponseNotFound
+from django.http.response import HttpResponse, HttpResponseNotFound
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+
+from rest_framework.request import Request
 from .serializer import PostSerializer
 from rest_framework.generics import GenericAPIView
 from .serializer import PostSerializer, CreatePostSerializer
 from authors.models import Author
+from .models import Post, Visibility
 from rest_framework import status
 import logging
 logger = logging.getLogger("mylogger")
+class PublicPostView(GenericAPIView):
+    def get_queryset(self):
+        return Post.objects.all()
+
+    def get(self, request: Request, *args, **kwargs) -> HttpResponse:
+        try:
+            public_posts = Post.objects.filter(
+                visibility = Visibility.PUBLIC
+            )
+        except Exception as e:
+            logger.info(e)
+            return HttpResponseNotFound()
+        serializer = PostSerializer(public_posts, many=True)
+        return Response(serializer.data)
 
 class CreationPostView(GenericAPIView):
     serializer_class = CreatePostSerializer
