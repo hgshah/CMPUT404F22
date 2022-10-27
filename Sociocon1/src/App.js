4a986728504046ecd0918798d38e6c3810bec49c
turnipxenon <turnipxenon@gmail.com>
2022-10-27 16:36:37 -0600

    Wrap main app in state manager or persistor
    Add TestFetcher.tsx back

--- Sociocon1/src/App.js
+++ Sociocon1/src/App.js
@@ -1,37 +1,43 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from './Home';
-import Sidebar from './Sidebar';
-import Feed from './Feed';
-import News from './News';
 import Login from './Login';
 import Profile from './Profile';
 import Inbox from './Inbox';
+import store, {persistor} from "./store";
+import {PersistGate} from "redux-persist/integration/react";
+import {Provider} from "react-redux";
+import "./App.css";
+
+
 function App() {
-  return (
-    // bem
-    <Router>
-        <div className="app">
-        {/* link: https://www.youtube.com/watch?v=Ul3y1LXxzdU
-            author: https://www.youtube.com/c/WebDevSimplified 
+    return (
+        // bem
+        <Provider store={store}>
+            <PersistGate persistor={persistor} loading={null}>
+                <Router>
+                    <div className="app">
+                        {/* link: https://www.youtube.com/watch?v=Ul3y1LXxzdU
+            author: https://www.youtube.com/c/WebDevSimplified
             License: https://creativecommons.org/choose/ */}
-            
-              <Routes>
-                <Route path = "/" element = {<Home/>} />
-                <Route path = "/login" element = {<Login/>} />
-                <Route path = "/profile" element = {<Profile/>} />
-                <Route path = "/inbox" element = {<Inbox/>} />
-              </Routes>
-              
-            
-       
-         </div>
+
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/profile" element={<Profile/>}/>
+                            <Route path="/inbox" element={<Inbox/>}/>
+                        </Routes>
 
 
-    </Router>
-     
-    
-  );
+                    </div>
+
+
+                </Router>
+            </PersistGate>
+        </Provider>
+
+
+    );
 }
 
 export default App;
--- Sociocon1/src/News.js
+++ Sociocon1/src/News.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./News.css";
+import {TestFetcher} from "./page/TestFetcher"
+
 function News() {
-  return (
+    return (
     <div className='news'>
       <h2> News</h2>
       <h2> Coming soon in next part!</h2>
+        {
+            /*
+            Hello! let me inject my TestFetcher!
+            Do not delete. We'll use this as reference!
+            */
+            TestFetcher()
+        }
     </div>
   )
 }
