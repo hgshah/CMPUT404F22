28e2289b14d866dc0c76e087def3f33360682fbe
harkiratmalhi <hsmalhi@ualberta.ca>
2022-10-28 12:01:16 -0600

    inbox-interface

--- Sociocon1/src/App.js
+++ Sociocon1/src/App.js
@@ -1,8 +1,25 @@
+import { Route, Routes, NavLink, Outlet, BrowserRouter, Navigate } from "react-router-dom";
+import React,{useState} from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import News from './News';
+import Inbox from './Inbox';
+import Profile from "./Profile";
+
 function App() {
+  const [isMenu, setisMenu] = useState(false);
+  const [isResponsiveclose, setResponsiveclose] = useState(false);
+  const toggleClass = () => {
+  };
+
+  let boxClass = ["nav__container"];
+
+  if(isMenu) {
+    boxClass.push('responsive__nav__show');
+  }else{
+    boxClass.push('');
+  }
   return (
     // bem
     <div className="app">
@@ -12,7 +29,23 @@ function App() {
       {/*feed */}
       <Feed />
       {/*widgets */}
-      <News />
+      <News /> 
+
+      {/* <ul className={boxClass.join(' ')} >
+        
+        <li><NavLink onClick={toggleClass} className={({ isActive }) => isActive ? "active" : ""} to="/Profile">Profile</NavLink></li>
+        <li><NavLink onClick={toggleClass} className={({ isActive }) => isActive ? "active" : ""} to="/Inbox">Inbox</NavLink></li>
+        <Outlet />
+      </ul> */}
+
+      {/* <Routes>
+        <Route path="/profile" element={<Profile />} /> */}
+        
+        {/* Approach #2 */}
+        {/*inbox*/}
+        {/* <Route path="/inbox" element={<Inbox />} />
+      </Routes> */}
+
     </div>
   );
 }
