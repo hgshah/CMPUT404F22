79b5b3e3335b94196692e437f521dbf35d18a08c
turnipxenon <turnipxenon@gmail.com>
2022-12-02 04:06:16 -0700

    Fix React deployment to Heroku

--- local.sh
+++ local.sh
@@ -1,13 +1,20 @@
+# Locally, our npm build is inside Socioecon1, and should not be in the root folder
+# But for Heroku deployment, it's actually in the root folder so this runs differently
+
+set -e
+
 # Django but local
 python mysocial/manage.py migrate --settings mysocial.settings.local
 
 # Respect existing environment variables
+cd Sociocon1
 export SOCIOCON_TEMP_VAR=$BUILD_PATH
 export BUILD_PATH="../mysocial/mysocial/staticfiles/"
-npm run --prefix Sociocon1 build
+npm install
+npm run build
 export BUILD_PATH=$SOCIOCON_TEMP_VAR
+cd ..
 
 # .keep!
 touch mysocial/mysocial/staticfiles/.keep
-
-python mysocial/manage.py runserver
+python mysocial/manage.py runserver # might run into issue inside python env
