23b4902368019efae7342b538adfd8600f52c9d3
turnipxenon <turnipxenon@gmail.com>
2022-12-02 01:46:41 -0700

    Add logs in release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,15 @@
 # Django
+echo "Starting release"
 python mysocial/manage.py migrate --settings mysocial.settings.production
+echo "Database migrated"
 
 # Respect existing environment variables
 export SOCIOCON_TEMP_VAR=$BUILD_PATH
 export BUILD_PATH="../mysocial/mysocial/staticfiles/"
 npm run build Sociocon1
+echo "React app built"
 export BUILD_PATH=$SOCIOCON_TEMP_VAR
 
 # .keep!
 touch mysocial/mysocial/staticfiles/.keep
+echo "Release finished"
