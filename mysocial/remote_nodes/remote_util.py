f7c7eef84c9b48d93bd0ee2d752e9e51f351f438
turnipxenon <turnipxenon@gmail.com>
2022-12-02 20:38:25 -0700

    Add team12 metadata

--- mysocial/remote_nodes/remote_util.py
+++ mysocial/remote_nodes/remote_util.py
@@ -39,6 +39,7 @@ class RemoteUtil:
     REMOTE_WIP_TAG = 'remote wip'
     TEAM14_CONNECTED = 'team14 connected'
     TEAM7_CONNECTED = 'team7 connected'
+    TEAM12_CONNECTED = 'team12 connected'
 
     REMOTE_NODE_SINGLE_PARAMS = [
         OpenApiParameter(name=NODE_TARGET_QUERY_PARAM, location=OpenApiParameter.QUERY,
